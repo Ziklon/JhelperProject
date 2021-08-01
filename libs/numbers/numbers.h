be95d52a18a973bc85cbcfaa751571f1b54568c5
Wilber Torres <winftc@gmail.com>
2021-08-01 11:50:25 +0200

    Adding numbers lib

---
+++ libs/numbers/numbers.h
@@ -0,0 +1,45 @@
+#pragma once
+
+#include "commons.h"
+
+template<typename T>
+T gcd(T a, T b) {
+  a = abs(a);
+  b = abs(b);
+  while (b != 0) {
+    a = a % b;
+    swap(a, b);
+  }
+  return a;
+}
+
+template<typename T>
+T lcm(T a, T b) {
+  return a / gcd(a, b) * b;
+}
+
+template<typename T>
+T power(const T a, ll b) {
+  if (b == 0) {
+    return 1;
+  }
+  if ((b & 1) == 0) {
+    T res = power(a, b >> 1);
+    return res * res;
+  } else {
+    return power(a, b - 1) * a;
+  }
+}
+
+template<typename T>
+T power(const T a, ll b, const ll md) {
+  if (b == 0) {
+    return 1;
+  }
+  if ((b & 1) == 0) {
+    T res = power(a, b >> 1);
+    return res * res % md;
+  } else {
+    return power(a, b - 1) * a % md;
+  }
+}
--- testrunner/main.cpp
+++ testrunner/main.cpp
@@ -1,4 +1,4 @@
-#include "../tasks/MessageRoute.cpp"
+#include "../tasks/ESafetyJourney.cpp"
 
 #include <iostream>
 #include <fstream>
@@ -35,7 +35,7 @@ int main() {
     string yellow = "\x1B[33m";
     string def = "\033[0m";
     std::vector<jhelper::Test> tests = {
-		{"5 5\n1 2\n1 3\n1 4\n2 3\n5 4\n", "3\n1 4 5\n", true, true},
+		{"3 1 4\n2 3\n", "4\n", true, true},{"3 3 3\n1 2\n1 3\n2 3\n", "0\n", true, true},{"5 3 100\n1 2\n4 5\n2 3\n", "428417047\n", true, true},
 	};
 	bool allOK = true;
 	int testID = 0;
@@ -67,7 +67,7 @@ int main() {
                 in = Input(cin);
                 //std::stringstream in(test.input);
                 out.setOut(fout);
-                MessageRoute solver;
+                ESafetyJourney solver;
                 solver.solve();
                 out.flush();
                 fout.close();
